7a7cbe9e5d819541776017af7509a0f3060350ef
Dowhyj <dowhyjlucas@gmail.com>
2025-04-12 11:18:49 -0300

    Agrego poda

--- maxi_subconjunto.py
+++ maxi_subconjunto.py
@@ -1,31 +1,36 @@
-def maxi_subconjunto(matriz, k, sub_conj, start):
+def calcular_valores_maximos(matriz):
+    valores = []
+    n = len(matriz)
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                valores.append(matriz[i][j])
+    return sorted(valores, reverse=True)
+
+def maxi_subconjunto(matriz, k, sub_conj, start, suma_actual, mejor_sum, mejor_sub_conj, valores_maximos):
     
-    #Caso base
-    if len(sub_conj)==k:
-        suma = 0
+    # Poda
+    faltan = k - len(sub_conj)
+    max_extra = sum(valores_maximos[:faltan*faltan])
+    if suma_actual + max_extra <= mejor_sum[0]:
+        return
+
+    # Caso base
+    if len(sub_conj) == k:
+        if suma_actual > mejor_sum[0]:
+            mejor_sum[0] = suma_actual
+            mejor_sub_conj[0] = sub_conj[:]
+        return
         
-        for i in sub_conj:
-            for j in sub_conj:
-                suma += matriz[i][j]
-        
-        return suma, sub_conj[:]
-    
-    mejor_sum = 0
-    mejor_sub_conj = []
-        
-    #Paso recursivo
+    # Paso recursivo
     for i in range(start, len(matriz)):
+        nuevo_valor = 0
+        for j in sub_conj:
+            nuevo_valor += matriz[i][j] + matriz[j][i]
+
         sub_conj.append(i)
-        suma_actual, subconj_actual = maxi_subconjunto(matriz, k, sub_conj, i+1)
-
-        if suma_actual > mejor_sum:
-            mejor_sum = suma_actual
-            mejor_sub_conj = subconj_actual[:]
-
-        
-        sub_conj.pop()        
-    
-    return mejor_sum, mejor_sub_conj
+        maxi_subconjunto(matriz, k, sub_conj, i+1, suma_actual + nuevo_valor, mejor_sum, mejor_sub_conj, valores_maximos)
+        sub_conj.pop()  # Backtrack
     
 #Input
 n = int(input('Ingrese el orden de la matriz: '))
@@ -38,8 +43,13 @@ for i in range(n):
 k = int(input('Ingrese el valor de k: '))
 sub_conj = []     #Lista donde voy a ir guardando los posibles indices
 
-#LLamada principal
-mejor_sum, mejor_sub_conj = maxi_subconjunto(matriz, k, sub_conj, 0)  
+mejor_sum = [0]
+mejor_sub_conj = [[]]
 
-print("Mejor subconjunto:", mejor_sub_conj)
-print("Suma máxima:", mejor_sum)
+valores_maximos = calcular_valores_maximos(matriz)
+
+#LLamada principal
+maxi_subconjunto(matriz, k, sub_conj, 0, 0, mejor_sum, mejor_sub_conj, valores_maximos)
+
+print("Mejor subconjunto de índices:", mejor_sub_conj[0])
+print("Suma máxima:", mejor_sum[0])
