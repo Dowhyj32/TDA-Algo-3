2c4053f56e01b5a60fe1c809b016a1cff285af47
Dowhyj32 <dowhyjlucas@gmail.com>
2025-04-21 12:24:56 -0300

    apilacion top-down

--- PilaCauta.py
+++ PilaCauta.py
@@ -3,7 +3,7 @@ s = [15,13,7,8,2]
 N = len(w)
 
 
-
+'''
 def backtraking(i, peso_actual, soporte_actual):
     
     if i == N:
@@ -18,4 +18,39 @@ def backtraking(i, peso_actual, soporte_actual):
         
     return max(sin_i, con_i)
 
-print(backtraking(0,0,float('inf')))
+print(backtraking(0,0,float('inf')))'''
+
+memo = {}
+
+def pila_td(i, soporte, memo):
+    
+    if i == N:
+        return (0, [])
+    
+    if (i, soporte) in memo:
+        return memo[(i, soporte)]
+    
+    sin_i_valor, sin_i_cajas = pila_td(i+1, soporte, memo)
+    
+    con_i_valor = 0
+    con_i_cajas = []
+    
+    if w[i]<soporte:
+        nuevo_soporte = min(soporte-w[i], s[i])
+        sig_valor, sig_cajas = pila_td(i+1, nuevo_soporte, memo)
+        con_i_valor = 1 + sig_valor
+        con_i_cajas = [i] + sig_cajas
+        
+    if con_i_valor > sin_i_valor:
+        memo[(i,soporte)] = (con_i_valor, con_i_cajas)
+    
+    else:
+        memo[(i,soporte)] = (sin_i_valor, sin_i_cajas)
+        
+    return memo[(i, soporte)]
+    
+res, cajas_usadas = pila_td(0, float('inf'), memo)
+
+print(f"Máximo número de cajas: {res}")
+print(f"Índices de cajas utilizadas: {cajas_usadas}")
+print(f"Pesos de cajas utilizadas: {[w[i] for i in cajas_usadas]}")
