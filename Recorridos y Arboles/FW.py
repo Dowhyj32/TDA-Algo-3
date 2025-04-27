0528f79fc9a014ad158cb76e59a3525943ece0c8
Dowhyj <dowhyjlucas@gmail.com>
2025-04-27 09:29:43 -0300

    Floyd-Warshall

---
+++ Recorridos y Arboles/FW.py
@@ -0,0 +1,17 @@
+def floyd_warshall(grafo):
+    """
+    grafo: matriz de adyacencia donde grafo[i][j] es el peso de la arista i -> j
+    Si no hay arista directa entre i y j, se asume grafo[i][j] = +inf.
+    """
+
+    n = len(grafo)
+    # Inicializar la matriz de distancias
+    dist = [fila[:] for fila in grafo]  # Copia profunda
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][k] + dist[k][j] < dist[i][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+
+    return dist
