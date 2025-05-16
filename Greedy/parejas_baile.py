2344fed8e53694e7d94a02d0b8e471a329e26b4b
Dowhyj32 <dowhyjlucas@gmail.com>
2025-05-16 11:02:45 -0300

    Agrego funcion recursiva

--- Greedy/parejas_baile.py
+++ Greedy/parejas_baile.py
@@ -3,7 +3,7 @@ def parejas_baile(A,B):
     parejas = 0
     parejas_formadas = []
     
-    while i<len(A) and j<len(B):
+    while i<len(A) and j<len(B):    #O(n+m): n=len(A) m=len(B)
         if abs(A[i]-B[i])<=1:
             parejas +=1
             parejas_formadas.append((i,j))
@@ -20,8 +20,23 @@ def parejas_baile(A,B):
     return parejas, parejas_formadas
         
 
+def parejas_baile_recursivo(A,B,i=0,j=0):
+    if i>=len(A) or j>=len(B):
+        return 0, []
+    
+    if abs(A[i]-B[i])<=1:
+        parejas, lista = parejas_baile_recursivo(A,B,i+1,j+1)
+        return 1 + parejas, [(i,j)]+lista
+    
+    elif A[i]<B[j]:
+        return parejas_baile_recursivo(A,B,i+1,j)
+    
+    else:
+        return parejas_baile_recursivo(A,B,i,j+1)        
+        
+        
 A = [1,2,4,6]    
 B = [1,5,5,7,9]
-parejas, parejas_formadas = parejas_baile(A,B)
+parejas, parejas_formadas = parejas_baile_recursivo(A,B)
 print(f'Total de parejas: {parejas}') 
 print(f'Parejas formadas: {parejas_formadas}')
