7f69a613a81c7ba3e06d5790b637b3e82e4d504e
Dowhyj32 <dowhyjlucas@gmail.com>
2025-05-19 11:07:56 -0300

    BFS

---
+++ Recorridos y Arboles/BFS.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def BFS(grafo, inicio):
+    visitados = set()
+    cola = deque([inicio])
+    res = []
+    
+    visitados.add(inicio)
+    
+    while cola:
+        nodo = cola.popleft()
+        res.append(nodo)
+        
+        for vecino in grafo[nodo]:
+            if vecino not in visitados:
+                visitados.add(vecino)
+                cola.append(vecino)
+                
+    return res
+    
+grafo = {
+    1: [2,4],
+    2: [1,3,5,7,8],
+    3: [2,4,9,10],
+    4: [1,3],
+    5: [2,6,7,8],
+    6: [5],
+    7: [2,5,8],
+    8: [2,5,7],
+    9: [3],
+    10: [3]
+}
+
+print(BFS(grafo, 1))
