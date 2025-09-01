ac72c0b049ec155b10a0dca6e044393047265ca9
Dowhyj32 <dowhyjlucas@gmail.com>
2025-08-31 21:54:39 -0300

    Listo

--- 2ºC 2025/Dividiar y conquistar/cazador_falsos.py
+++ 2ºC 2025/Dividiar y conquistar/cazador_falsos.py
@@ -1,9 +1,20 @@
-def cazador_falsos(matriz, i, j):
+'''def cazador_falsos(matriz, i, j):
 
     if len(matriz)==1:
         return 
     
     medio = (i+j)//2
 
+    if len(matriz)<=1:
+        return matriz[i][j]
     
+    #Si no hay ningun falso, sigo diviendo
+    elif conjuncion_Submatriz():
+        return cazador_falsos(matriz,)
+    '''
     
+m = [[1,2,3]]
+m2 = [[1,2,3],[4,5,6]]
+
+print(len(m)) 
+print(len(m2))
